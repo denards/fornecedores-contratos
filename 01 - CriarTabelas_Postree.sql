f0772838cdbdc983a4ffc7ec2d4cb5f906dc69fe
Denard Soares <denard@sognisport.com.br>
2025-02-22 14:11:23 -0300

    adicao das procedures altera_valor_licitacao

--- 01 - CriarTabelas_Postree.sql
+++ 01 - CriarTabelas_Postree.sql
@@ -121,3 +121,80 @@ $$ LANGUAGE plpgsql;
 CREATE TRIGGER trg_auditoria_licitacao
 AFTER INSERT OR UPDATE OR DELETE ON licitacao
 FOR EACH ROW EXECUTE FUNCTION auditoria_licitacao();
+
+--procedure para alterar o valor da licitação
+CREATE OR REPLACE PROCEDURE public.altera_valor_licitacao(IN p_licitacao_id bigint, IN p_novo_valor numeric)
+ LANGUAGE plpgsql
+AS $procedure$
+DECLARE
+    v_valor_atual    NUMERIC(15, 2);
+BEGIN
+    -- 1) Obter o valor atual da licitação
+    SELECT valor
+      INTO v_valor_atual
+      FROM licitacao
+     WHERE id = p_licitacao_id;
+
+    -- 2) Verifica se existe a licitação e se há realmente mudança no valor
+    IF v_valor_atual IS NULL THEN
+        RAISE NOTICE 'Licitação % não encontrada.', p_licitacao_id;
+        RETURN;
+    END IF;
+
+    IF v_valor_atual <> p_novo_valor THEN
+        -- 3) Atualiza o valor na tabela licitacao
+        UPDATE licitacao
+           SET valor = p_novo_valor
+         WHERE id = p_licitacao_id;
+
+        -- 4) Insere registro na tabela auditoria
+        INSERT INTO auditoria(
+            licitacao_id,
+            valor_total_antigo,
+            valor_total_novo,
+            operacao,
+            alterado_em,
+            usuario
+        )
+        VALUES(
+            p_licitacao_id,          -- licitacao_id
+            v_valor_atual,           -- valor_total_antigo
+            p_novo_valor,            -- valor_total_novo
+            'UPDATE_VALOR',          -- nome/tipo da operação
+            CURRENT_TIMESTAMP,       -- data/hora da mudança
+            CURRENT_USER             -- usuário atual
+        );
+
+        RAISE NOTICE 'Valor da licitação % alterado de % para %.',
+            p_licitacao_id, v_valor_atual, p_novo_valor;
+    ELSE
+        RAISE NOTICE 'Valor da licitação % já é %. Nenhuma atualização realizada.',
+            p_licitacao_id, v_valor_atual;
+    END IF;
+END;
+$procedure$
+;
+--procedure para alterar a situação da licitação
+CREATE OR REPLACE PROCEDURE public.atualiza_situacao_licitacao(IN lic_id bigint, IN nova_situacao character varying)
+ LANGUAGE plpgsql
+AS $procedure$
+BEGIN
+    IF nova_situacao = 'Cancelada' THEN
+        UPDATE licitacao
+        SET situacao = nova_situacao
+        WHERE id = lic_id;
+        
+        RAISE NOTICE 'Licitação % foi cancelada.', lic_id;
+        
+    ELSIF nova_situacao = 'Em andamento' THEN
+        UPDATE licitacao
+        SET situacao = nova_situacao
+        WHERE id = lic_id;
+        
+        RAISE NOTICE 'Licitação % está em andamento.', lic_id;
+        
+    ELSE
+        RAISE NOTICE 'Situação não reconhecida. Nenhuma atualização foi feita.';
+    END IF;
+END;
+$procedure$
